bd82ecd74111a4b62abe897def7cd3bd99b491b0
Landfrasz Ádám <adam.landfrasz@gmail.com>
2020-05-19 22:47:48 +0200

    created Notes component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+
+import Note from "./Note";
 
 function Notes() {
-  return <div></div>;
+  const [cookies] = useCookies(["user"]);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/notes`, { withCredentials: true })
+      .then((res) => setNotes(res.data.userNotes))
+      .catch((e) => console.log(e));
+  }, []);
+  console.log(notes);
+  return cookies.user ? (
+    <React.Fragment>
+      <h4>NOTES</h4>
+      <div>
+        {notes.map((note) => (
+          <Note key={note._id} note={note}></Note>
+        ))}
+      </div>
+    </React.Fragment>
+  ) : (
+    <Redirect to="/login" />
+  );
 }
 
 export default Notes;
