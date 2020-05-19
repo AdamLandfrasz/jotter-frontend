49de773b18f32b33d03cc66aacd0b9fff4346fce
Landfrasz Ádám <adam.landfrasz@gmail.com>
2020-05-19 22:48:13 +0200

    implemented Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
 function Login() {
-  return <div></div>;
+  const [cookies, setCookie] = useCookies(["user"]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/auth/login`,
+        { email, password },
+        { withCredentials: true }
+      );
+      if (response.data.success) {
+        const user = response.data.existingUser;
+        setCookie("user", { id: user._id, email: user.email });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return cookies.user ? (
+    <Redirect to="/" />
+  ) : (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        autoComplete="off"
+        name="email"
+        id="login-email"
+        placeholder="E-mail"
+        required
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        id="login-password"
+        placeholder="Password"
+        required
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">SIGN IN</button>
+    </form>
+  );
 }
 
 export default Login;
