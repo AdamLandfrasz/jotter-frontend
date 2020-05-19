dbca016813a04e950f2cb273e95e8428ffec70d8
Landfrasz Ádám <adam.landfrasz@gmail.com>
2020-05-19 21:36:28 +0200

    added cookie context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
+import Notes from "./components/Notes";
+import Login from "./components/Login";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [cookies] = useCookies([]);
+
   return (
     <div className="App">
-      <Container fluid="md"></Container>
+      <Container fluid="md">
+        <Router>
+          <Switch>
+            <Route path="/" component={cookies.user ? Notes : Login} />
+          </Switch>
+        </Router>
+      </Container>
     </div>
   );
 }
