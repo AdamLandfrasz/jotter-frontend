c9a99aeabc4e55187090ad82b5cbb3ad00515c6b
Landfrasz Ádám <adam.landfrasz@gmail.com>
2020-06-02 20:32:39 +0200

    not found page minor change

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <React.Fragment>
       <div>404 NOT FOUND</div>
-      <Link to="/">HOME</Link>
+      <a href="/">HOME</a>
     </React.Fragment>
   );
 };
